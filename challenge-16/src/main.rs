20478884222807d9a261bbfb505fedee2ab416cb
Justin Doan <ivigideon25@gmail.com>
2020-07-26 00:25:23 -0400

    Added Time Passed in seconds

--- challenge-16/src/main.rs
+++ challenge-16/src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
+use std::time::Instant;
 
 fn main() {
+    let start_time = Instant::now();
     let args: Vec<String> = env::args().collect();
     let string_to_reverse = args.get(1);
     if string_to_reverse.unwrap_or(&"".to_owned()).chars().count() == 0 {
         println!("Please Enter an argument string!")
     } else {
         let reversed_string: String = string_to_reverse.unwrap().chars().rev().collect();
+        let time_elasped: f64 = start_time.elapsed().as_nanos() as f64 / 1000000000 as f64;
         println!("{}", reversed_string);
+        println!("Time taken in seconds: {}", time_elasped)
     }
 }
